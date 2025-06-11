1bfe3584451f3f72712ab85a82314157de3a2103
GAYATHRI1006 <gayathrigaya3ece@gmail.com>
2025-06-11 17:03:10 +0530

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import { useState } from 'react'
+import './App.css'
+import QrCode from './Qrcode'
+
+function App() {
+ 
+  return (
+    <>
+      <QrCode/>
+    </>
+  )
+}
+
+export default App
